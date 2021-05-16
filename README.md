f3208af438a038262608f21525ecd3046eaec713
YazanSneneh <yaz.sneneh@gmail.com>
2021-05-16 19:52:05 +0300

    add task home and all tasks activities

---
+++ README.md
@@ -0,0 +1,22 @@
+# Task Master 
+* Create home page Activity.
+* Create Task Activity.
+* Create All tasks Activity.
+
+#### Brief Discription
+* home
+  * add task button take user to add activity screen
+  * all tasks button take user to view all tasks activity screen
+* add activity screen
+  * add task button make submit new event
+
+
+### Screen Shots
+  * home
+  ![home](/screenshots/Home_Activity.jpeg)
+
+  * Add Task
+  ![home](/screenshots/add_task.jpeg)
+
+  * All Tasks
+  ![home](/screenshots/all_tasks.jpeg)
--- app/src/main/java/com/firstapp/taskmaster/MainActivity.java
+++ app/src/main/java/com/firstapp/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.firstapp.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get button by id
+        // add event to button
+        // call method inside event
+            // then inside method use intent object from class
+            // provide it the current class and distination class
+            //
+        Button addTask = findViewById(R.id.button1);
+
+        addTask.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                addTasks();
+            }
+        });
+
+
+        Button allTasks = findViewById(R.id.button2);
+
+        allTasks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                viewAllTasks();
+            };
+        });
+
     }
+
+    protected void addTasks(){
+        Intent intent = new Intent(this, AddTask.class);
+        startActivity(intent);
+    }
+
+    protected void viewAllTasks(){
+        Intent intent = new Intent(this, AllTasks.class);
+        startActivity(intent);
+    };
 }
